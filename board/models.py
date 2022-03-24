f04cbdf7b53482f031a920d8f01e81931825b1ee
takoyaki-junior <takoyakijunior5@gmail.com>
2022-03-25 00:28:30 +0900

    20220324 "semantic UI コミット"

--- board/models.py
+++ board/models.py
@@ -1,6 +1,59 @@
 from django.db import models
 from accounts.models import User
 
+# トピックのステータス一覧
+TOPIC_STATUS_CHOICE = (
+    (0, '公開'),
+    (1, '非公開'),
+    (2, '承認待ち'),
+)
+
+# トピックのステータス一覧
+COMMENT_STATUS_CHOICE = (
+    (0, '公開'),
+    (1, '非公開'),
+)
+
+
+class ThreadManager(models.Manager):
+    # Thread操作に関する処理を追加
+    pass
+
+
+class CommentManager(models.Manager):
+    # Comment操作に関する処理を追加
+    pass
+
+
+class CategoryManager(models.Manager):
+    # Category操作に関する処理を追加
+    pass
+
+
+class Category(models.Model):
+    name = models.CharField(
+        'カテゴリー名',
+        max_length=50,
+    )
+    url_code = models.CharField(
+        'URLコード',
+        max_length=50,
+        null=True,
+        blank=False,
+        unique=True,
+    )
+    sort = models.IntegerField(
+        verbose_name='ソート',
+        default=0,
+    )
+    objects = CategoryManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'category'
+
 
 class Thread(models.Model):
 
@@ -10,11 +63,17 @@ class Thread(models.Model):
     content = models.TextField(
         verbose_name='内容', blank=True, null=True)
     created_by = models.ForeignKey(
-        User, on_delete=models.PROTECT)
+        User, verbose_name='投稿者', on_delete=models.PROTECT)
+    category = models.ForeignKey(
+        Category,  verbose_name='カテゴリー', on_delete=models.PROTECT, null=True, blank=False)
+    status = models.IntegerField(
+        verbose_name='トピックのステータス', default=0, choices=TOPIC_STATUS_CHOICE)
     created_at = models.DateTimeField(
         verbose_name='作成日時', auto_now_add=True)
     updated_at = models.DateTimeField(
         verbose_name='更新日時', auto_now=True)
+
+    objects = ThreadManager()
 
     def __str__(self):
         return self.title
@@ -26,16 +85,23 @@ class Thread(models.Model):
 class Comment(models.Model):
 
     # カラム
+    id = models.BigAutoField(
+        primary_key=True)
+    no = models.IntegerField(default=0)
     comment = models.TextField(
         verbose_name='コメント', blank=False, null=False)
     created_by = models.ForeignKey(
-        User, on_delete=models.PROTECT)
+        User, verbose_name='スレッド', on_delete=models.PROTECT)
     thread = models.ForeignKey(
-        Thread, on_delete=models.CASCADE)
+        Thread, verbose_name='投稿者', on_delete=models.CASCADE)
+    status = models.IntegerField(
+        verbose_name='コメントのステータス', default=0, choices=COMMENT_STATUS_CHOICE)
     created_at = models.DateTimeField(
         verbose_name='作成日時', auto_now_add=True)
     updated_at = models.DateTimeField(
         verbose_name='更新日時', auto_now=True)
+
+    objects = CommentManager()
 
     def __str__(self):
         return self.message
--- board/urls.py
+++ board/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 app_name = "board"
@@ -7,6 +8,6 @@ urlpatterns = [
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/update/', views.update, name='update'),
     path('<int:pk>/delete/', views.delete, name='delete'),
-
     path('create/', views.create, name='create'),
+    path('terms/', TemplateView.as_view(template_name='base/terms.html'), name='terms'),
 ]
